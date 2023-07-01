02eb46ae7b423f6e09d8029ade6c1945aa7c78eb
Preet81196 <31180792+Preet81196@users.noreply.github.com>
2023-07-01 12:52:41 -0400

    ex-2

---
+++ Java-script/ex-2-javascript/largest-num.js
@@ -0,0 +1,15 @@
+function LargestNumber(num1, num2, num3) {
+  var largest = num1;
+
+  if (num2 > largest) {
+    largest = num2;
+  }
+
+  if (num3 > largest) {
+    largest = num3;
+  }
+
+  return largest;
+}
+
+console.log(LargestNumber(12, 13, 14));
---
+++ Java-script/ex-2-javascript/odd-or-Even.js
@@ -0,0 +1,9 @@
+function oddEven(number) {
+  if (number % 2 === 0) {
+    return "this is even number";
+  } else {
+    return "odd number";
+  }
+}
+
+console.log(oddEven(15));
